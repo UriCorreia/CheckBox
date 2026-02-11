78cc86d2dc996148f1d34bfda491db7869aebf4c
Uriel-CdS <uriel2405correia@gmail.com>
2026-02-11 17:53:03 -0300

    ConnectionBD

--- src/main/java/br/com/uriel/database/ConnectionFactory.java
+++ src/main/java/br/com/uriel/database/ConnectionFactory.java
@@ -1,4 +1,16 @@
 package br.com.uriel.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+    private static final String URL = "jdbc:postgresql://localhost:5432/db_tarefas";
+    private static final String usuario = "postgres";
+    private static final String senha = "uri24";
+
+    // Método de conexão com o banco de dados
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, usuario, senha);
+    }
 }
--- src/main/java/br/com/uriel/database/ConnectionTest.java
+++ src/main/java/br/com/uriel/database/ConnectionTest.java
@@ -1,4 +1,13 @@
 package br.com.uriel.database;
 
 public class ConnectionTest {
+    public static void main (String[] args){
+        try {
+            ConnectionFactory.getConnection();
+            System.out.println("Banco conectado com sucesso!");
+        } catch (Exception e){
+            System.out.println("Banco não conectado, conexão não estabelecida com sucesso!");
+            e.printStackTrace();
+        }
+    }
 }
